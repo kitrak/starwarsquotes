ecb472f19bfd638d99e6bf3adca6b8f8f427b3bf
Kartik Subramanian <kartik.subram@gmail.com>
2017-02-27 22:09:18 -0800

    Add POST route for quotes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 
 app.listen(3000, function() {
     console.log("Listening on 3000")
-});
+})
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
+	res.sendFile(__dirname + '/index.html')
+})
 
+app.post('/quotes', (req, res) => {
+	console.log("Hellooooooooo!")
+})
