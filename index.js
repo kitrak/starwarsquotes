de174d7c3c2673a877eb65da7c43bd9e32ee296a
Kartik Subramanian <kartik.subram@gmail.com>
2017-02-27 21:56:24 -0800

    Use ES6

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.listen(3000, function() {
     console.log("Listening on 3000")
-    });
+});
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-})
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
 
